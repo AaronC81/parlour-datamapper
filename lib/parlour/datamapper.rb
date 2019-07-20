8d99080471f7679004270a9c1864ecabfcbe5b1b
Aaron Christiansen <aaronc20000@gmail.com>
2019-07-20 23:16:54 +0100

    Initial commit

---
+++ lib/parlour/datamapper.rb
@@ -0,0 +1,8 @@
+require "parlour/datamapper/version"
+
+module Parlour
+  module Datamapper
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/parlour/datamapper/version.rb
@@ -0,0 +1,5 @@
+module Parlour
+  module Datamapper
+    VERSION = "0.1.0"
+  end
+end
